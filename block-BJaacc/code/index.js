6bd745fb77683b83aecb6bca44eb08cf2242d3ae
achyuth yadav <achyuthyadav@hmail.com>
2022-01-27 12:04:53 +0530

    refactor

--- block-BJaacc/code/index.js
+++ block-BJaacc/code/index.js
@@ -8,10 +8,10 @@ let students = {
 
 students.studentName = "achyuth"
 
-
+ 
 // 3. Using `console.log` log the value of `studentName` key from the `students` object
 
-console.log(students[studentName])
+console.log(students.studentName)
 
 // 4. Add a another key of the value of variable `batch` in the object with the value of 16.
 
@@ -19,19 +19,19 @@ students.batch = 16 ;
 
 // 5. Using `console.log` log the value of `batch` key from the `students` object
 
-console.log(students[batch])
+console.log(students.batch);
 
 // 6. Add another key in the object with the value of the variable `isAdult`. The value of the key should be `true/false`
 
-
+students.isAdult = true;
 
 // 7. Using `console.log` print the value of `isAdult` key from the `students` object
 
-console.log(students[isAdult])
+console.log(students.isAdult)
 
 // 8. Add a another key of `42` to the object with a value of `The answer to the meaning of life`.
 
-students.[42]= `The answer to the meaning of life`;
+students[42]= `The answer to the meaning of life`;
 
 // 9. Using `console.log` print the value of key `42` from the `students` object
 
@@ -40,14 +40,18 @@ consolelog(students[42])
 
 // 10. Check the length of object named `students`.
 
+console.log(students.length)
 
 
 // 11. Can you define a key of `let or var` in any object? Reason.
 
-
+students["let"] = "Any Value"
 
 // 12. Delete the `isAdult` property from the object `students`. Also, print the object before or after deleting the property
 
-
+delete students.isAdult;
+console.log(students);
 
 // 13. Update the value of the key batch in the object `students` and print it using `console.log`
+students.batch =18 ;
+console.log(students.batch);
